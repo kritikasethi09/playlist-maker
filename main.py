01d705c0c82dbfce6f29813955f49b74563b7234
kritikasethi09 <kritikasethi@Kritikas-MacBook-Air.local>
2023-08-01 18:51:33 -0400

    extracting data

--- main.py
+++ main.py
@@ -2,29 +2,61 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
+import pandas as pd
 
+#extract info from env file
 load_dotenv()
-
 client_id = os.environ.get('CLIENT_ID')
 client_secret = os.environ.get('CLIENT_SECRET')
 redirect_uri = os.environ.get('REDIRECT_URI')
 
+#deal with authentication/authorization
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, 
-redirect_uri="http://localhost:8888/callback", scope='playlist-read-private'))
-def get_playlist_tracks(playlist_id):
-    results = sp.playlist_tracks(playlist_id)
-    tracks = results['items']
+redirect_uri="http://localhost:8888/callback", scope='playlist-read-private user-top-read playlist-modify-private'))
 
-    while results['next']:
-        results = sp.next(results)
-        tracks.extend(results['items'])
+#everything playlist
+playlist_link = "https://open.spotify.com/playlist/5jbbLJqtKuqhRENtQFdrqi?si=8018f59b99b44813"
+playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
 
-    return tracks
+track_data = []
 
+for track in sp.playlist_tracks(playlist_URI)["items"]:
+    track_uri = track["track"]["uri"]
+    track_name = track["track"]["name"]
+    artist_name = track["track"]["artists"][0]["name"]
+    track_pop = track["track"]["popularity"]
 
-playlist_id = '5jbbLJqtKuqhRENtQFdrqi'
-tracks = get_playlist_tracks(playlist_id)
-for idx, track in enumerate(tracks, start=1):
-    track_name = track['track']['name']
-    track_artist = track['track']['artists'][0]['name']
-    print(f"{idx}. {track_name} - {track_artist}")
+    audio_features = sp.audio_features(track_uri)[0]
+    acousticness = audio_features['acousticness']
+    danceability = audio_features['danceability']
+    energy = audio_features['energy']
+    instrumentalness = audio_features['instrumentalness']
+    key = audio_features['key']
+    loudness = audio_features['loudness']
+    mode = audio_features['mode']
+    speechiness = audio_features['speechiness']
+    tempo = audio_features['tempo']
+    time_signature = audio_features['time_signature']
+    valence = audio_features['valence']
+
+    track_data.append({
+    'Track Name': track_name,
+    'Artist Name': artist_name,
+    'Popularity': track_pop,
+    'Acousticness': acousticness,
+    'Danceability': danceability,
+    'Energy': energy,
+    'Instrumentalness': instrumentalness,
+    'Key': key,
+    'Loudness': loudness,
+    'Mode': mode,
+    'Speechiness': speechiness,
+    'Tempo': tempo,
+    'Time Signature': time_signature,
+    'Valence': valence,
+    })
+df = pd.DataFrame(track_data)
+with open('raw_data.txt', 'w') as output:
+    output.write('Raw Data:\n')
+    output.write(df.to_string(index=False))
